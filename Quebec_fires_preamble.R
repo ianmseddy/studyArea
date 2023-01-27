623736198d989d216c6402345a7704d0d517cab3
Alex Chubaty <achubaty@for-cast.ca>
2023-01-26 20:40:51 -0700

    add custom standAgeMap

--- Quebec_fires_preamble.R
+++ Quebec_fires_preamble.R
@@ -54,7 +54,9 @@ defineModule(sim, list(
     createsOutput("rasterToMatchReporting", "RasterLayer", desc = "Raster to match based on studyAreaReporting."),
     createsOutput("sppColorVect", "character", desc = NA),
     createsOutput("sppEquiv", "data.table", desc = NA),
-    createsOutput("sppEquivCol", "data.table", desc = NA),
+    createsOutput("sppEquivCol", "character", desc = "name of column to use in `sppEquiv`."),
+    createsOutput("standAgeMap2001", "RasterLayer", desc = "raster of time since disurbance for year 2001."),
+    createsOutput("standAgeMap2011", "RasterLayer", desc = "raster of time since disurbance for year 2011."),
     createsOutput("studyArea", "SpatialPolygons", desc = "Buffered study area in which to run simulations."),
     createsOutput("studyAreaLarge", "SpatialPolygons", desc = "Buffered study area used for parameterization/calibration."),
     createsOutput("studyAreaPSP", "SpatialPolygonsDataFrame",
@@ -102,8 +104,7 @@ Init <- function(sim) {
                                             "Pice_gla", "Pice_mar",
                                             "Pinu_ban",
                                             "Popu_tre"), ]
-  sim$sppColorVect <- LandR::sppColors(sppEquiv = sim$sppEquiv, sppEquivCol = sim$sppEquivCol,
-                                       palette = "Accent")
+  sim$sppColorVect <- LandR::sppColors(sim$sppEquiv, sim$sppEquivCol, newVals = "Mixed", palette = "Accent")
 
   return(invisible(sim))
 }
@@ -255,6 +256,73 @@ Init <- function(sim) {
   sim$studyArea <- st_transform(sim$studyArea, crs = crs(sim$rasterToMatch)) |> as_Spatial()
   sim$studyAreaLarge <- st_transform(sim$studyAreaLarge, crs = crs(sim$rasterToMatchLarge)) |> as_Spatial()
   sim$studyAreaReporting <- st_transform(sim$studyAreaReporting, crs = crs(sim$rasterToMatchReporting)) |> as_Spatial()
+
+  ## standAgeMaps
+  standAgeMapURL <- paste0(
+    "http://ftp.maps.canada.ca/pub/nrcan_rncan/Forests_Foret/",
+    "canada-forests-attributes_attributs-forests-canada/2001-attributes_attributs-2001/",
+    "NFI_MODIS250m_2001_kNN_Structure_Stand_Age_v1.tif"
+  )
+  standAgeMapFileName <- basename(standAgeMapURL)
+
+  fireURL <- "https://cwfis.cfs.nrcan.gc.ca/downloads/nfdb/fire_poly/current_version/NFDB_poly.zip"
+
+  fireYear <- Cache(prepInputsFireYear,
+                    earliestYear = 1950,
+                    url = fireURL,
+                    destinationPath = dPath,
+                    rasterToMatch = sim$rasterToMatchLarge)
+
+  fireYear <- postProcess(fireYear, rasterToMatch = sim$rasterToMatchLarge) ## needed cropping
+
+  standAgeMap2001 <- Cache(
+    LandR::prepInputsStandAgeMap,
+    ageURL = standAgeMapURL,
+    rasterToMatch = sim$rasterToMatchLarge,
+    studyArea = sim$studyAreaLarge,
+    destinationPath = dPath,
+    startTime = 2001,
+    fireFun = "terra::vect",
+    fireURL = fireURL,
+    filename2 = .suffix("standAgeMap_2001.tif", paste0("_", P(sim)$studyAreaName)),
+    userTags = c("stable", currentModule(sim), P(sim)$studyAreaName)
+  )
+
+  standAgeMap2011 <- Cache(
+    LandR::prepInputsStandAgeMap,
+    ageURL = paste0("https://ftp.maps.canada.ca/pub/nrcan_rncan/Forests_Foret/",
+                    "canada-forests-attributes_attributs-forests-canada/",
+                    "2011-attributes_attributs-2011/",
+                    "NFI_MODIS250m_2011_kNN_Structure_Stand_Age_v1.tif"),
+    rasterToMatch = sim$rasterToMatchLarge,
+    studyArea = sim$studyAreaLarge,
+    destinationPath = dPath,
+    startTime = 2011,
+    fireFun = "terra::vect",
+    fireURL = fireURL,
+    filename2 = .suffix("standAgeMap_2011.tif", paste0("_", P(sim)$studyAreaName)),
+    userTags = c("stable", currentModule(sim), P(sim)$studyAreaName)
+  )
+
+  ## stand age maps already adjusted within fire polygons using LandR::prepInputsStandAgeMap.
+  ## now, adjust pixels which are younger than oldest fires upward
+  earliestFireYear <- as.integer(minValue(fireYear))
+
+  minNonDisturbedAge2001 <- 2001L - earliestFireYear
+  toChange2001 <- is.na(fireYear[]) & standAgeMap2001[] <= minNonDisturbedAge2001
+  standAgeMap2001[toChange2001] <- minNonDisturbedAge2001 + 2L ## make it an even 40 years old instead of 39
+  imputedPixID2001 <- unique(attr(standAgeMap2001, "imputedPixID"), which(toChange2001))
+
+  minNonDisturbedAge2011 <- 2011L - earliestFireYear
+  toChange2011 <- is.na(fireYear[]) & standAgeMap2011[] <= minNonDisturbedAge2011
+  standAgeMap2011[toChange2011] <- minNonDisturbedAge2011 + 2L ## make it an even 50 years old instead of 49
+  imputedPixID2011 <- unique(attr(standAgeMap2011, "imputedPixID"), which(toChange2011))
+
+  sim$standAgeMap2001 <- asInteger(standAgeMap2001)
+  attr(sim$standAgeMap2001, "imputedPixID") <- imputedPixID2011
+
+  sim$standAgeMap2011 <- asInteger(standAgeMap2011)
+  attr(sim$standAgeMap2011, "imputedPixID") <- imputedPixID2011
 
   return(invisible(sim))
 }
